80167b625df5d25eb397e7911f2001fd83a7ff78
jonas.batista <jonas.batista@spotmetrics.com>
2025-08-05 21:14:13 -0300

    MediaMatch component

--- .jest/setup.js
+++ .jest/setup.js
@@ -1,3 +1,17 @@
 // .jest/setup.js
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import media, { DefaultBreakpoints } from 'styled-media-query';
+
+type Breakpoint = keyof DefaultBreakpoints;
+
+export type MediaMatchProps = {
+  greaterThan?: Breakpoint;
+  lessThan?: Breakpoint;
+};
+
+const createMediaMatchModifiers = () => ({
+  lessThan: (size: Breakpoint) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+  greaterThan: (size: Breakpoint) => css`
+    ${media.greaterThan(size)`
+      display: block;
+    `}
+  `,
+});
+
+const mediaMatchModifiers = createMediaMatchModifiers();
+
+const MediaMatch = styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`;
+
+export default MediaMatch;
